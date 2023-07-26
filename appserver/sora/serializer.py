06dc13618686f07979e96722d255e68eb444e011
Polar <tpwns73@gmail.com>
2023-07-26 17:27:04 +0900

    간단한 수정

---
+++ appserver/sora/serializer.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import *
+from django.contrib.auth.hashers import make_password
+
+class UserSerializer(serializers.ModelSerializer):
+    def create(self, valiated_date):
+        valiated_date['user_pw'] = make_password(valiated_date['user_pw'])
+        user = UserInfo.objects.create(**valiated_date)
+        return user
+    
+    def validate(self, attrs):
+        return attrs
+    
+    class Meta:
+        model = UserInfo
+        fields = ("user_id", "email", "user_pw", "gender", "phone_number",
+                "university", "student_id", "department", "description",
+                "create_at", "update_at", "auth")
